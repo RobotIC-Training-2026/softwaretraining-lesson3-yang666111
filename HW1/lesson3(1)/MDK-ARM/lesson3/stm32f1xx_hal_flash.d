dae9e46ca232da701119b25ebc908eb0470c81cb
yang666111 <3482617358@qq.com>
2025-10-28 16:11:42 +0800

    串口通信作业

---
+++ HW1/lesson3(1)/MDK-ARM/lesson3/stm32f1xx_hal_flash.d
@@ -0,0 +1,27 @@
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+lesson3\stm32f1xx_hal_flash.o: ../Core/Inc/stm32f1xx_hal_conf.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Include/core_cm3.h
+lesson3\stm32f1xx_hal_flash.o: D:\APP\APP install\Keil MDK533 install\ARM\ARMCC\Bin\..\include\stdint.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Include/cmsis_version.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+lesson3\stm32f1xx_hal_flash.o: D:\APP\APP install\Keil MDK533 install\ARM\ARMCC\Bin\..\include\stddef.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+lesson3\stm32f1xx_hal_flash.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
---
+++ HW2/lesson3(2)/MDK-ARM/lesson3(2)/main.d
@@ -0,0 +1,30 @@
+lesson3(2)\main.o: ../Core/Src/main.c
+lesson3(2)\main.o: ../Core/Inc/main.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+lesson3(2)\main.o: ../Core/Inc/stm32f1xx_hal_conf.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Include/core_cm3.h
+lesson3(2)\main.o: D:\APP\APP install\Keil MDK533 install\ARM\ARMCC\Bin\..\include\stdint.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Include/cmsis_version.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+lesson3(2)\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+lesson3(2)\main.o: D:\APP\APP install\Keil MDK533 install\ARM\ARMCC\Bin\..\include\stddef.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+lesson3(2)\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
+lesson3(2)\main.o: D:\APP\APP install\Keil MDK533 install\ARM\ARMCC\Bin\..\include\stdio.h
+lesson3(2)\main.o: D:\APP\APP install\Keil MDK533 install\ARM\ARMCC\Bin\..\include\string.h
